c608958018b42fcbf77d1ffe42bb06b97b2fc668
Julia Strapassan <juliastrapa@gmail.com>
2026-03-25 17:27:47 -0300

    Adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,14 +4,19 @@ import { sleep, check } from 'k6';
 
 
 export const options = {
-    vus: 10,
-    duration: '30s',
+    stages:[
+        {duration: '5', target: 10 },
+        {duration: '20s', target: 10 },
+        {duration: '5', target: 0 }
+    ],
+
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
     }
-    
 }
+    
+
 
 export default function () {
     const url = 'http://localhost:3000/login';
